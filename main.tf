518e9e6b568a4cd8a1a0373c12c2621783a0e6d5
Liam Ray <liamrayx@gmail.com>
2023-11-05 00:17:03 +0200

    - more automation for ovpn server

--- main.tf
+++ main.tf
@@ -1,11 +1,30 @@
-provider "aws" {
-  region = "eu-central-1"
+variable "region" {
+  default = "ca-central-1"
 }
 
-data "aws_ami" "ovpn_ami" {
+provider "aws" {
+  region = var.region
+}
+
+locals {
+  ovpn_remote_dir = "/home/ubuntu/ovpn-files"
+  ovpn_local_dir  = "${path.module}/output"
+}
+
+# searching for the ubuntu ami
+data "aws_ami" "ubuntu_ami" {
   most_recent = true
-  owners      = ["679593333241"] // openvpn market place
-  name_regex  = "OpenVPN Access Server QA Image-.*"
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 
@@ -18,6 +37,7 @@ data "aws_vpc" "default_vpc" {
   default = true
 }
 
+# openvpn ingress requirements: TCP (443, 943, 945), UDP (1194). we also temporary open a 22 port for different initializations
 resource "aws_security_group" "ovpn_sgs" {
   description = "OVPN SGs"
   vpc_id      = data.aws_vpc.default_vpc.id
@@ -74,47 +94,145 @@ resource "aws_security_group" "ovpn_sgs" {
   }
 }
 
+# some regions have only t3.micro instance type, but preffer to use a t2.micro as possible
+data "aws_ec2_instance_type_offering" "instance_type" {
+  filter {
+    name   = "instance-type"
+    values = ["t2.micro", "t3.micro"]
+  }
+
+  preferred_instance_types = ["t2.micro"]
+}
+
+# Create an IAM policy
+resource "aws_iam_policy" "revoke_ingress_rule_policy" {
+  name = "revoke-ingress-rule-policy-for-ovpn-server"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : "ec2:RevokeSecurityGroupIngress",
+        "Resource" : "arn:aws:ec2:${var.region}:${data.aws_caller_identity.current_account.account_id}:security-group/${aws_security_group.ovpn_sgs.id}"
+      }
+    ]
+  })
+}
+
+# Create an IAM role
+resource "aws_iam_role" "revoke_ingress_rule_role" {
+  name = "revoke-ingress-rule-role-for-ovpn-server"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          "Service" : "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach the IAM policy to the IAM role
+resource "aws_iam_policy_attachment" "revoke_ingress_attachment" {
+  name       = "revoke-ingress-rule-attachment-for-OVPN-server"
+  policy_arn = aws_iam_policy.revoke_ingress_rule_policy.arn
+  roles      = [aws_iam_role.revoke_ingress_rule_role.name]
+}
+
+resource "aws_iam_instance_profile" "ovpn_instance_profile" {
+  name = "ovpn-ec2-instance-profile"
+  role = aws_iam_role.revoke_ingress_rule_role.name
+}
+
+
+# the ec2 instance
 resource "aws_instance" "ovpn_server" {
-  ami                    = data.aws_ami.ovpn_ami.id
-  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.ubuntu_ami.id
+  instance_type          = data.aws_ec2_instance_type_offering.instance_type.instance_type
   key_name               = aws_key_pair.my_kp.id
   vpc_security_group_ids = [aws_security_group.ovpn_sgs.id]
+  user_data              = file("${path.module}/user_data.sh")
+
+  iam_instance_profile = aws_iam_instance_profile.ovpn_instance_profile.name
 
   tags = {
     Name = "ovpn-server"
   }
 }
 
+# the permanent ip address
 resource "aws_eip" "ovpn_elastic_ip" {
   instance = aws_instance.ovpn_server.id
 }
 
+data "aws_caller_identity" "current_account" {}
 
-output "summary" {
-  value = "First SSH to the server and setup it: ssh openvpnas@${aws_eip.ovpn_elastic_ip.public_ip} . And then enter to the portal and configure users: https://${aws_eip.ovpn_elastic_ip.public_ip}/admin"
+data "aws_iam_session_context" "current_user" {
+  arn = data.aws_caller_identity.current_account.arn
 }
 
+resource "null_resource" "wait_for_aws_instance" {
+  depends_on = [aws_instance.ovpn_server]
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+
+    host = aws_eip.ovpn_elastic_ip.public_ip
+  }
+
+  # waiting for the profile.ovpn and credentials.txt files to be produced...
+  provisioner "remote-exec" {
+    inline = [
+      "#!/bin/bash",
+      "ovpn_remote_dir=${local.ovpn_remote_dir}",
+      file("${path.module}/wait-for-files.sh")
+    ]
+  }
+
+  # making a local.ovpn_base_dir local dir
+  provisioner "local-exec" {
+    command = "mkdir -p '${local.ovpn_local_dir}'"
+  }
+
+  # copying files from the remote dir to the local dir
+  provisioner "local-exec" {
+    command = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_rsa ubuntu@${aws_eip.ovpn_elastic_ip.public_ip}:${local.ovpn_remote_dir}/* ${local.ovpn_local_dir}"
+  }
+
+  # deleting files in the remote dir
+  provisioner "local-exec" {
+    command = "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ~/.ssh/id_rsa ubuntu@${aws_eip.ovpn_elastic_ip.public_ip} sudo rm ${local.ovpn_remote_dir}/*"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "#!/bin/bash",
+      "ovpn_sg_name=${aws_security_group.ovpn_sgs.name}",
+      file("${path.module}/block-ssh.sh")
+    ]
+  }
+}
+
+output "summary" {
+  value = "The OVPN server is up and ready. The profile and credentials file are in the [${local.ovpn_local_dir}] directory. Enjoy ;-)"
+}
 
 /*
-# you can use a command line to create a new user and produce an ovpn file
-# and disable a web ui (you can also block a 443 and 943 ports in sgs as well to block a web ui)
-
-cd /usr/local/openvpn_as/scripts
-sudo su
-
-username="<username>"
-password="<password>"
-
-# creating a new user
-./sacli --user ${username} --key "type" --value "user_connect" UserPropPut
-./sacli --user ${username} --new_pass ${password} SetLocalPassword
-
-# generating an ovpn file
-./sacli --prefer-tls-crypt-v2 --user ${username} GetUserlogin > /home/openvpnas/${username}.ovpn
-
-# disabling web ui
-./sacli --key "admin_ui.https.ip_address" --value lo ConfigPut
-./sacli start
-
-
+generate key-pair and use it instead of my key-pair
+  https://stackoverflow.com/questions/55745961/connection-issue-using-ssh-private-key
+  https://stackoverflow.com/questions/48567881/how-can-i-use-terraforms-file-provisioner-to-copy-from-my-local-machine-onto-a
+user_data supply env vars somehow
+test in all regions
+use launch template instead of aws_instance, set desired = min = max instance = 1
 */
